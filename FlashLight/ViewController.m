7f31411bdfba54d10929a809ffbb6c75d48271f5
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2016-02-23 08:32:00 +0900

    画面処理を実装します。

--- FlashLight/ViewController.m
+++ FlashLight/ViewController.m
@@ -33,11 +33,81 @@
     [super didReceiveMemoryWarning];
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self becomeFirstResponder];
+}
+
 - (IBAction)performLightSwitchAction:(id)sender
 {
+    // ライト制御を実行します。
+    [self configureLight];
 }
 
 - (IBAction)performBrightnessSliderAction:(id)sender
 {
+    // ライト制御を実行します。
+    [self configureLight];
 }
+
+/**
+ *  もージョンイベント終了時の処理です。
+ *
+ *  @param motion モーションです。
+ *  @param event  イベントです。
+ */
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    // シェイクジェスチャを認識した時の処理です。
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        // スイッチのON/OFFを切り替えます。
+        [self.lightSwitch setOn:!self.lightSwitch.isOn animated:YES];
+        
+        // ライト制御を実行します。
+        [self configureLight];
+    }
+}
+
+/**
+ *  ファーストレスポンダとして設定します。
+ *
+ *  @return ファーストレスポンダの有効フラグ。
+ */
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)configureLight
+{
+    NSError *error = nil;
+    
+    // ビデオデバイスを取得します。
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    // 各コントローラから値を取得します。
+    BOOL isOn = self.lightSwitch.isOn;
+    float level = self.brightnessSlider.value;
+    
+    // トーチを持ったデバイスが正しく取得できた場合の処理です。
+    if (device && device.hasTorch) {
+        // デバイスをロックします。
+        [device lockForConfiguration:&error];
+        
+        if (level > 0.0f && isOn) {
+            // トーチの明るさを設定します。
+            [device setTorchModeOnWithLevel:level error:&error];
+        }
+        else
+        {
+            // トーチをオフにします。
+            device.torchMode = AVCaptureTorchModeOff;
+        }
+        
+        // ロックを解除します。
+        [device unlockForConfiguration];
+    }
+}
+
 @end
